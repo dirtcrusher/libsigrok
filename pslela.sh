794c9376d59f34e7fd1d52186cc7a1e341cdfbe5
Rob Bresseleers <rob.bresseleers@ensimag.grenoble-inp.fr>
2018-01-26 11:18:09 +0100

    Added compilation script for integration in PSLELA

---
+++ pslela.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# https://stackoverflow.com/a/16349776
+cd "${0%/*}"
+
+doxygen Doxyfile
+RET=$?
+if [ $RET -ne 0 ]; then
+	exit $RET
+fi
+
+./autogen.sh
+RET=$?
+if [ $RET -ne 0 ]; then
+	exit $RET
+fi
+
+./configure --disable-all-drivers --enable-pslela --enable-fx2lafw
+RET=$?
+if [ $RET -ne 0 ]; then
+	exit $RET
+fi
+
+make -j 4
+RET=$?
+if [ $RET -ne 0 ]; then
+	exit $RET
+fi
+
